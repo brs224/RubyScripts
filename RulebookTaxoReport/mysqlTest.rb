9aa82543e13f43c2cf7744c8bbd86a050fb3f0e3
Barbara.Switzer <barbara.switzer@thomsonreuters.com>
2015-02-12 15:34:05 -0500

    Ruby REport Script

---
+++ RulebookTaxoReport/mysqlTest.rb
@@ -0,0 +1,17 @@
+require 'mysql2'
+
+
+begin
+    client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "Barb8514")
+    #puts con.get_server_info
+	#result = client.query('CALL sp_customer_list( 25, 10 )')
+    rs = client.query('SELECT VERSION()')
+    puts rs.first   
+    
+rescue Mysql2::Error => e
+    puts e.errno
+    puts e.error
+    
+ensure
+    client.close if client
+end
